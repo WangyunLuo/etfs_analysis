86c252d874386449da98427bdfdb094845f07a37
WangyunLuo <WangyunLuo@users.noreply.github.com>
2020-05-20 11:55:34 -0400

    Initial launch

---
+++ analysis.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wangyun Luo
+email: samlwy@bu.edu
+"""
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+from datetime import timedelta
+from datetime import date
+
+
+# ---------------------------------------------------------------------------------------
+""" download data """
+""" change df to analysis """
+ticker = 'tsla'
+time_interv = timedelta(days=365 * 5)
+# min_date_allowed = datetime(2015, 4, 1)
+df = yf.download(f'{ticker}', date.today()-time_interv, date.today())
+""" write file to store data """
+# data.to_csv(raw_data_path)
+df_adj_close = df['Adj Close']
+df_volume = df['Volume']
+# df_volume.plot()
+# ---------------------------------------------------------------------------------------
+""" MA """
+df = yf.download(f'{ticker}', date.today()-time_interv, date.today())
+print('adding min_periods=0 did the same thing as df.dropna')
+df['10D MA'] = df['Adj Close'].rolling(window=10, min_periods=0).mean()
+print(df.head())
+# df.dropna(inplace=True)
+# print(df.head())
+""" resample """
+df_ohlc = df['Adj Close'].resample('1M').ohlc()
+""" Visualization """
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=2, colspan=1)
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['10D MA'])
+ax2.plot(df.index, df['Volume'])
+plt.show()
+
--- app1.py
+++ app1.py
@@ -13,7 +13,7 @@ import numpy as np
 import random
 from args import tickers, indicators, raw_data_path, input_data_path, processed_data_path
 from process_data import cal_df_stats, cal_df_stats_multiprocess
-import timedelta
+from datetime import timedelta
 from datetime import date
 import multiprocessing
 from multiprocessing import Pool
@@ -24,9 +24,9 @@ import dash_auth
 
 # ---------------------------------------------------------------------------------------
 # app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-USERNAME_PASSWORD_PAIRS = [['wangyun', '0415']]
+# USERNAME_PASSWORD_PAIRS = [['wangyun', '0415']]
 app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
-auth = dash_auth.BasicAuth(app, USERNAME_PASSWORD_PAIRS)
+# auth = dash_auth.BasicAuth(app, USERNAME_PASSWORD_PAIRS)
 server = app.server
 # ---------------------------------------------------------------------------------------
 options = [{'label': tic, 'value': tic} for tic in tickers]
@@ -35,13 +35,11 @@ options = [{'label': tic, 'value': tic} for tic in tickers]
 # time_interv = timedelta(days=365 * 5)
 min_date_allowed = datetime(2015, 4, 1)
 data = yf.download(tickers, min_date_allowed, date.today())
-# data.to_csv(raw_data_path)
 input_data = data['Adj Close']
 volumn_data = data['Volume']
-# input_data.to_csv(input_data_path)
+# ---------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------
 # process the data
-# df = pd.read_csv(input_data_path, index_col=0)
 processed_data = cal_df_stats(input_data, tickers, indicators)
 # --------------------------------- Multiprocessing ---------------------------------
 # df = input_data
@@ -61,13 +59,25 @@ df = processed_data
 # ---------------------------------------------------------------------------------------
 app.layout = html.Div(
     [
+
+        # html.H1('ETFs Dashboard', style={'margin'}),
+        html.Img(
+            src='https://images.squarespace-cdn.com/content/5c036cd54eddec1d4ff1c1eb/1557908564936-YSBRPFCGYV2CE43OHI7F/GlobalAI_logo.jpg?content-type=image%2Fpng',
+            style={
+                'height': '11%',
+                'width': '11%',
+                'float': 'right',
+                'position': 'relative',
+                'margin-top': 11,
+                'margin-right': 0}, className='two columns'
+        ),
         html.H1("ETFs Dashboard", style={'text-align': 'center'}),
         html.Hr(),
         html.Div([
             dbc.Row([
                 dbc.Col(html.Div([html.H3('Enter a ETFs ticker:', style={'paddingRight': '30px'}),
-                                  dcc.Dropdown(id='my-ticker', value='XLV', options=options)],
-                                 style={'verticalAlign': 'top', 'display': 'inline-block'})),
+                                  dcc.Dropdown(id='my-ticker', value='XLV', options=options)])),
+                # style={'verticalAlign': 'top', 'display': 'inline-block'})),
                 dbc.Col(html.Div([html.H3('Select Date:'),
                                   dcc.DatePickerRange(id='date-range',
                                                       min_date_allowed=datetime(
@@ -83,15 +93,22 @@ app.layout = html.Div(
                    block=True, id="button", className="mb-3"),
         html.Hr(),
         dbc.Row([
-            dbc.Col(dcc.Graph(id="trend"), style={
-                'border-style': 'solid'}, width=6),
+            # dbc.Col(dcc.Graph(id="trend"), style={
+            #     'border-style': 'solid'}, width=6),
             dbc.Col(dcc.Graph(id="timeseries"), style={
-                'border-style': 'solid'}, width=6),
+                'border-style': 'solid'}),
         ]),
         html.Hr(),
         dbc.Row([
             dbc.Col(dcc.Graph(id="table"),  style={
                 'border-style': 'solid'}),
+        ]),
+        html.Hr(),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id="volatility"), style={
+                'border-style': 'solid'}, width=6),
+            dbc.Col(dcc.Graph(id="momentum"),  style={
+                'border-style': 'solid'}, width=6),
         ]),
         html.Hr(),
         dbc.Row([
@@ -115,92 +132,91 @@ app.layout = html.Div(
 
 
 @ app.callback(
-    [Output("trend", "figure"),
-     Output("timeseries", "figure"),
-     Output("scatter", "figure"),
-     Output("histogram", "figure"),
-     Output("bar", "figure"),
-     Output("table", "figure"),
-     Output("heatmap", "figure"),
-     Output("radar", "figure"),
-     Output("speedometer", "figure")],
+    [  # Output("trend", "figure"),
+        Output("timeseries", "figure"),
+        Output("table", "figure"),
+        Output("volatility", "figure"),
+        Output("momentum", "figure"),
+        Output("histogram", "figure"),
+        Output("bar", "figure"),
+        Output("scatter", "figure"),
+        Output("heatmap", "figure"),
+        Output("radar", "figure"),
+        Output("speedometer", "figure")],
     [Input("button", "n_clicks")],
     [State("my-ticker", "value"),
      State("date-range", "start_date"),
      State("date-range", "end_date")])
 def update_trend1_graph(n_clicks, etfs_ticker, start_date, end_date):
-    # start = datetime.strptime(start_date[: 10], "%Y-%m-%d")
-    # end = datetime.strptime(end_date[: 10], "%Y-%m-%d")
     df = processed_data.loc[(processed_data.index >= start_date) & (
         processed_data.index <= end_date)]
-    # df = yf.download(etfs_ticker, start_date, end_date)
-    # processed_df = cal_df_stats(df, etfs_ticker, indicators)
     # -----------------------------------------------------------------------------------
     # update data if necessary
-    # spy = df['SPY']
 
     # -----------------------------------------------------------------------------------
-    # ticker_indicators = [
-    #     '{}_'.format(etfs_ticker) + indicator for indicator in indicators]
-    # filtered_df = df[ticker_indicators]
-    # # -------------------- set up the output trace --------------------------------------
-    # traces = []
-    # traces = [go.Scatter(
-    #     x=filtered_df.index,
-    #     y=filtered_df[col],
-    #     mode='lines',
-    #     name=col
-    # ) for col in ticker_indicators]
-    traces = [
+    # traces = [
+    #     go.Scatter(x=df.index, y=df[etfs_ticker],
+    #                mode="lines", name=etfs_ticker),
+    #     go.Scatter(x=df.index, y=df[etfs_ticker].rolling(
+    #         20).mean(), mode="lines", name=f"{etfs_ticker} 20_D_MA"),
+    #     go.Scatter(x=df.index, y=df["SPY"],
+    #                mode="lines", name="SPY"),
+    #     go.Scatter(x=df.index, y=df["SPY"].rolling(
+    #         20).mean(), mode="lines", name="SPY 20_D_MA"),
+    # ]
+    # trend = go.Figure(data=traces,
+    #                   layout=go.Layout(title=dict(text=f"{etfs_ticker} & SPY",
+    #                                               xanchor='center',
+    #                                               x=0.5),
+    #                                    xaxis=dict(title='Date'),
+    #                                    yaxis=dict(title='Price'),
+    #                                    paper_bgcolor="#222",
+    #                                    plot_bgcolor='#222',
+    #                                    font=dict(color='#fff')))
+
+    # time series of both selected_ticker & SPY
+    timeseries_data = [
         go.Scatter(x=df.index, y=df[etfs_ticker],
-                   mode="lines", name=etfs_ticker),
+                   mode="lines", name=f'{etfs_ticker}'),
+        go.Scatter(x=df.index, y=df['SPY'], mode="lines", name='SPY'),
         go.Scatter(x=df.index, y=df[etfs_ticker].rolling(
             20).mean(), mode="lines", name=f"{etfs_ticker} 20_D_MA"),
-        go.Scatter(x=df.index, y=df["SPY"],
-                   mode="lines", name="SPY"),
         go.Scatter(x=df.index, y=df["SPY"].rolling(
             20).mean(), mode="lines", name="SPY 20_D_MA"),
+        go.Scatter(x=df.index, y=df[etfs_ticker].rolling(
+            120).mean(), mode="lines", name=f"{etfs_ticker} 120_D_MA"),
+        go.Scatter(x=df.index, y=df["SPY"].rolling(
+            120).mean(), mode="lines", name="SPY 120_D_MA"),
     ]
-    # go.Indicator(value=df[etfs_ticker].iloc[-1],
-    #              mode="number+delta",
-    #              delta={
-    #                  'reference': df[etfs_ticker].iloc[-2], "valueformat": ".of"},
-    #              domain={'x': [0, 1], 'y': [0, 1]},
-    #              title={'text': 'Adj Close Price',
-    #                     'xanchor': 'center',
-    #                     'x': 0.5})
-    # 'xanchor': 'center', 'x': 0.5,
-    trend = go.Figure(data=traces,
-                      layout=go.Layout(title=dict(text=f"{etfs_ticker} & SPY",
-                                                  xanchor='center',
-                                                  x=0.5),
-                                       xaxis=dict(title='Date'),
-                                       yaxis=dict(title='Price'),
-                                       paper_bgcolor="#222",
-                                       plot_bgcolor='#222',
-                                       font=dict(color='#fff')))
 
-    # time series
-    timeseries = go.Figure(data=[go.Scatter(x=df.index, y=df[etfs_ticker])],
-                           layout=go.Layout(title=dict(text=f"{etfs_ticker} Time Series",
-                                                       xanchor='center',
+    timeseries = go.Figure(data=timeseries_data,
+                           layout=go.Layout(title=dict(text=f"{etfs_ticker}  SPY Time Series",
+                                                       xanchor='right',
                                                        x=0.5),
                                             xaxis=dict(title='Date'),
-                                            yaxis=dict(title='Price')))
+                                            yaxis=dict(title='Price'),
+                                            paper_bgcolor="#222",
+                                            plot_bgcolor='#222',
+                                            font=dict(color='#fff')))
     timeseries.update_xaxes(
         rangeslider_visible=True,
         rangeselector=dict(
             buttons=list([
-                dict(count=1, label="1m", step="month", stepmode="backward"),
-                dict(count=6, label="6m", step="month", stepmode="backward"),
-                dict(count=1, label="YTD", step="year", stepmode="todate"),
-                dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all")
-            ])))
+                dict(count=1, label="1m", step="month",
+                     stepmode="backward"),
+                dict(count=1, label="6m", step="month",
+                     stepmode="backward"),
+                dict(count=1, label="YTD", step="year",
+                     stepmode="todate"),
+                dict(count=1, label="1y", step="year",
+                     stepmode="backward"),
+                dict(step="all"),
+            ]),
+            font={'color': 'Black'}))
     timeseries.update_layout(title=f"{etfs_ticker} Time Series", paper_bgcolor="#222", plot_bgcolor="#222",
                              font={"color": "#fff"})
 
-    # scatter plot
+    # SPY and selected ticker return comparison
     scatter = go.Figure(data=[go.Scatter(x=df['SPY'].pct_change(),
                                          y=df[etfs_ticker].pct_change(),
                                          mode='markers',
@@ -214,11 +230,11 @@ def update_trend1_graph(n_clicks, etfs_ticker, start_date, end_date):
                                          paper_bgcolor="#222", plot_bgcolor="#222",
                                          font={"color": "#fff"}))
 
-    # histogram
+    # histogram to display the price distribution
     hist_data = [df[etfs_ticker].tolist(), df['SPY'].tolist()]
     group_labels = [etfs_ticker, "SPY"]
     hist = ff.create_distplot(hist_data, group_labels, bin_size=.2)
-    hist.update_layout(title=dict(text=f"{etfs_ticker} Price Distribution Histogram",
+    hist.update_layout(title=dict(text=f"{etfs_ticker} & SPY Price Distribution Histogram",
                                   xanchor='center',
                                   x=0.5),
                        xaxis={'title': 'Price'},
@@ -228,7 +244,7 @@ def update_trend1_graph(n_clicks, etfs_ticker, start_date, end_date):
                        font={"color": "#fff"})
 
     # horizontal Bar
-    # Calculate total volume of every month of the single stock and create dummy data
+    # volume comparison
     tic_bar = volumn_data[etfs_ticker].reset_index()
     tic_bar['Date'] = tic_bar['Date'].apply(
         lambda x: datetime.strftime(x, '%Y-%m'))
@@ -237,8 +253,7 @@ def update_trend1_graph(n_clicks, etfs_ticker, start_date, end_date):
     spy_bar['Date'] = spy_bar['Date'].apply(
         lambda x: datetime.strftime(x, '%Y-%m'))
     spy_bar = spy_bar["SPY"].groupby(spy_bar['Date']).mean()
-    # random_bar = pd.Series(data=np.random.randint(low=tic_bar.min(), high=tic_bar.max(), size=len(tic_bar)),
-    #                        index=tic_bar.index)
+
     bar = go.Figure(data=[go.Bar(y=tic_bar.index, x=tic_bar.tolist(), name=f"{etfs_ticker} Volume", orientation='h'),
                           go.Bar(y=spy_bar.index, x=spy_bar.tolist(), name="SPY Volume", orientation='h')],
                     layout=go.Layout(title=dict(text=f"{etfs_ticker} vs SPY Monthly Average Volume",
@@ -248,7 +263,7 @@ def update_trend1_graph(n_clicks, etfs_ticker, start_date, end_date):
                                      xaxis={'title': 'Volume'}, paper_bgcolor="#222", plot_bgcolor="#222",
                                      font={"color": "#fff"}))
 
-    # table
+    # table of all statistics for selected ticker
 
     tickers_indicators = [
         '{}_'.format(etfs_ticker) + indicator for indicator in indicators]
@@ -266,17 +281,52 @@ def update_trend1_graph(n_clicks, etfs_ticker, start_date, end_date):
                                        plot_bgcolor="#222",
                                        font={"color": "#fff"}))
 
-    # heatmap
-    # Calculate covariance matrix of the stock with other randomly chosen stocks
-    # random_list = random.sample(nasdq.index.tolist(), 10)
-    # if etfs_ticker in random_list:
-    #     random_list = random_list
-    # else:
-    #     random_list.append(etfs_ticker)
-    # matrix_df = yf.download(random_list, start, end)[
-    #     'Adj Close'].fillna(0).pct_change()
+    # volatility
+
+    volatility_data = [
+        go.Scatter(x=df.index, y=df[f'{etfs_ticker}_20D_Vol'],
+                   mode="lines", name=f'{etfs_ticker} 20D_Vol'),
+        go.Scatter(x=df.index, y=df[f'{etfs_ticker}_120D_Vol'],
+                   mode="lines", name=f"{etfs_ticker} 120D_MA"),
+        go.Scatter(x=df.index, y=df["SPY_20D_Vol"],
+                   mode="lines", name="SPY 20D_Vol"),
+        go.Scatter(x=df.index, y=df["SPY_120D_Vol"],
+                   mode="lines", name="SPY 120D_MA"),
+    ]
+    volatility = go.Figure(data=volatility_data,
+                           layout=go.Layout(title=dict(text=f"{etfs_ticker} & SPY volatility",
+                                                       xanchor='center',
+                                                       x=0.5),
+                                            xaxis=dict(title='Date'),
+                                            yaxis=dict(title='Volatility'),
+                                            paper_bgcolor="#222",
+                                            plot_bgcolor='#222',
+                                            font=dict(color='#fff')))
+    # momentum
+    momentum_data = [
+        go.Scatter(x=df.index, y=df[f'{etfs_ticker}_5D_Momentum'],
+                   mode="lines", name=f'{etfs_ticker} 5D_Momentum'),
+        go.Scatter(x=df.index, y=df[f'{etfs_ticker}_20D_Momentum'],
+                   mode="lines", name=f"{etfs_ticker} 20D_Momentum"),
+        go.Scatter(x=df.index, y=df["SPY_5D_Momentum"],
+                   mode="lines", name="SPY 5D_Momentum"),
+        go.Scatter(x=df.index, y=df["SPY_20D_Momentum"],
+                   mode="lines", name="SPY 20D_Momentum"),
+    ]
+    momentum = go.Figure(data=momentum_data,
+                         layout=go.Layout(title=dict(text=f"{etfs_ticker} & SPY momentum",
+                                                     xanchor='center',
+                                                     x=0.5),
+                                          xaxis=dict(title='Date'),
+                                          yaxis=dict(title='Momentum'),
+                                          paper_bgcolor="#222",
+                                          plot_bgcolor='#222',
+                                          font=dict(color='#fff')))
+
+    # correlation heatmap
+
     matrix_df = df[tickers].fillna(0)
-    # matrix_df = yf.download(tickers)['Adj Close'].fillna(0).pct_change()
+
     heatmap = go.Figure(data=[go.Heatmap(x=matrix_df.columns, y=matrix_df.columns,
                                          z=matrix_df.corr(), colorscale='Viridis')],
                         layout=go.Layout(title=dict(text="Correlation Heatmap of 11 ETFs",
@@ -286,14 +336,13 @@ def update_trend1_graph(n_clicks, etfs_ticker, start_date, end_date):
                                          font={"color": "#fff"}))
 
     # radar Chart
-    # Company Analysis (random data)
     criteria = ['Price', 'Leadership', 'Human Resources & Training', 'Organization for Change',
                 'Production System', 'Information & Communication Systems', 'Safety Practices',
                 'State of Technology', 'Subcontractor Management', 'Purchasing & Inventory Control',
                 'Understandable Goals', 'Planning Programming', 'Cost & Due Date Control']
     grades = list(np.random.randint(1, 11, size=len(criteria)))
     radar = go.Figure(data=[go.Scatterpolar(r=grades, theta=criteria, fill='toself')],
-                      layout=go.Layout(title=dict(text=f"{etfs_ticker} Company Analysis (Random Data)",
+                      layout=go.Layout(title=dict(text=f"{etfs_ticker} Analysis (Random Data)",
                                                   xanchor='center',
                                                   x=0.5),
                                        paper_bgcolor="#222", font={"color": "#fff"}))
@@ -308,11 +357,9 @@ def update_trend1_graph(n_clicks, etfs_ticker, start_date, end_date):
                                        plot_bgcolor="#222",
                                        font={"color": "#fff"}))
 
-    return trend, timeseries, scatter, hist, bar, table, heatmap, radar, speed
+    return timeseries, table, volatility, momentum, hist, bar, scatter, heatmap, radar, speed
 
 
 if __name__ == '__main__':
-    # pyo.plot(trend, filename='line1.html')
-    # pyo.plot(heatmap, filename='line2.html')
-    # pyo.plot(table, filename='line2.html')
+
     app.run_server()  # debug=True)
